decf5984d25dbb2e11727b650bfc69cca51c3844
Andrea La Commara <andrealacommara@icloud.com>
2025-11-08 16:25:17 +0100

    Aggiungi vite-plugin-sitemap per la generazione automatica della sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,19 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import VitePluginSitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    VitePluginSitemap({
+      hostname: "https://lacco.it",
+      dynamicRoutes: ["/la-mia-musica", "/su-di-me", "/contatti"],
+    }),
+    tsconfigPaths(),
+    tailwindcss(),
+    svgr(),
+  ],
   build: {
     rollupOptions: {
       output: {
