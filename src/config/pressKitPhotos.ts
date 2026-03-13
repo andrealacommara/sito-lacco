97e2ea5a40f9b6db83c70e5fddbd3e181deb8265
Andrea La Commara <andrealacommara@icloud.com>
2026-03-13 14:25:14 +0100

    feat: add new press kit photos, update pressKitPhotos configuration and update hero pic

--- src/config/pressKitPhotos.ts
+++ src/config/pressKitPhotos.ts
@@ -8,6 +8,15 @@ import pressKitPic5 from "@/assets/images/presskit/pressKitPic5.avif";
 import pressKitPic6 from "@/assets/images/presskit/pressKitPic6.avif";
 import pressKitPic7 from "@/assets/images/presskit/pressKitPic7.avif";
 import pressKitPic8 from "@/assets/images/presskit/pressKitPic8.avif";
+import pressKitPic9 from "@/assets/images/presskit/pressKitPic9.avif";
+import pressKitPic10 from "@/assets/images/presskit/pressKitPic10.avif";
+import pressKitPic11 from "@/assets/images/presskit/pressKitPic11.avif";
+import pressKitPic12 from "@/assets/images/presskit/pressKitPic12.avif";
+import pressKitPic13 from "@/assets/images/presskit/pressKitPic13.avif";
+import pressKitPic14 from "@/assets/images/presskit/pressKitPic14.avif";
+import pressKitPic15 from "@/assets/images/presskit/pressKitPic15.avif";
+import pressKitPic16 from "@/assets/images/presskit/pressKitPic16.avif";
+
 
 //Import of high quality press kit photos.
 import pressKitHQPic1 from "@/assets/images/presskit/HQ/pressKitPic1.jpg";
@@ -18,6 +27,14 @@ import pressKitHQPic5 from "@/assets/images/presskit/HQ/pressKitPic5.jpg";
 import pressKitHQPic6 from "@/assets/images/presskit/HQ/pressKitPic6.jpg";
 import pressKitHQPic7 from "@/assets/images/presskit/HQ/pressKitPic7.jpg";
 import pressKitHQPic8 from "@/assets/images/presskit/HQ/pressKitPic8.jpg";
+import pressKitHQPic9 from "@/assets/images/presskit/HQ/pressKitPic9.jpg";
+import pressKitHQPic10 from "@/assets/images/presskit/HQ/pressKitPic10.jpg";
+import pressKitHQPic11 from "@/assets/images/presskit/HQ/pressKitPic11.jpg";
+import pressKitHQPic12 from "@/assets/images/presskit/HQ/pressKitPic12.jpg";
+import pressKitHQPic13 from "@/assets/images/presskit/HQ/pressKitPic13.jpg";
+import pressKitHQPic14 from "@/assets/images/presskit/HQ/pressKitPic14.jpg";
+import pressKitHQPic15 from "@/assets/images/presskit/HQ/pressKitPic15.jpg";
+import pressKitHQPic16 from "@/assets/images/presskit/HQ/pressKitPic16.jpg";
 
 export const pressKitPhotos = [
   {
@@ -67,5 +84,54 @@ export const pressKitPhotos = [
     srcHQ: pressKitHQPic8,
     alt: "Lacco - Press Photo 8",
     full: pressKitHQPic8,
+  }
+  ,
+  {
+    src: pressKitPic9,
+    srcHQ: pressKitHQPic9,
+    alt: "Lacco - Press Photo 9",
+    full: pressKitHQPic9,
+  },
+  {
+    src: pressKitPic10,
+    srcHQ: pressKitHQPic10,
+    alt: "Lacco - Press Photo 10",
+    full: pressKitHQPic10,
+  },
+  {
+    src: pressKitPic11,
+    srcHQ: pressKitHQPic11,
+    alt: "Lacco - Press Photo 11",
+    full: pressKitHQPic11,
+  },
+  {
+    src: pressKitPic12,
+    srcHQ: pressKitHQPic12,
+    alt: "Lacco - Press Photo 12",
+    full: pressKitHQPic12,
+  },
+  {
+    src: pressKitPic13,
+    srcHQ: pressKitHQPic13,
+    alt: "Lacco - Press Photo 13",
+    full: pressKitHQPic13,
+  },
+  {
+    src: pressKitPic14,
+    srcHQ: pressKitHQPic14,
+    alt: "Lacco - Press Photo 14",
+    full: pressKitHQPic14,
+  },
+  {
+    src: pressKitPic15,
+    srcHQ: pressKitHQPic15,
+    alt: "Lacco - Press Photo 15",
+    full: pressKitHQPic15,
+  },
+  {
+    src: pressKitPic16,
+    srcHQ: pressKitHQPic16,
+    alt: "Lacco - Press Photo 16",
+    full: pressKitHQPic16,
   }
 ];
