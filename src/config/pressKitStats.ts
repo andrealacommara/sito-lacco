4ebe3b71a6a9c6930d30eab6d16c80e2b564d967
Andrea La Commara <andrealacommara@icloud.com>
2026-07-23 04:40:21 +0200

    fix: update press kit stats for accuracy

--- src/config/pressKitStats.ts
+++ src/config/pressKitStats.ts
@@ -1,7 +1,7 @@
 // ========================== PRESS KIT STATS ========================== //
 // Press Kit Page stats. To be updated monthly.
 export const pressKitStats = {
-  monthlyListeners: 826,
-  totalStreams: 53479,
-  spotifyFollowers: 155,
+  monthlyListeners: 292,
+  totalStreams: 57323,
+  spotifyFollowers: 164,
 };
